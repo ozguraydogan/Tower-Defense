91579797f27708855456a66fc8dc7fc0caa3f5b7
Özgür <61085808+ozguraydogan@users.noreply.github.com>
2021-06-24 13:02:49 +0300

    Start
    first three video

---
+++ TowerDefense/Assets/Scripts/Deneme.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Deneme : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
